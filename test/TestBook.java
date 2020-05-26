e5b2f6efc3bfdc4cfdf9266179be9f335b315d75
CodeDifferentlyGuides <65664324+CodeDifferentlyGuides@users.noreply.github.com>
2020-05-26 00:42:06 -0400

    Created TestBook,java

---
+++ test/TestBook.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void contructBookTest(){
+        String input "A tale of two cities";
+        String expected = "The title is: A tale of two cities";
+        String actual = "*insert main function call*";
+
+        Assert.assertEquals(expected, actual);
+    }
+}
